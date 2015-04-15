dd96e5042895ad72557da6dd5c2bbf886c926bbc
Adam Perelman <adamperelman@gmail.com>
2015-04-14 17:56:41 -0700

    refactoring and formatting

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 CXX = clang++
-CXXFLAGS += -mavx -std=c++11 -pedantic -O0 -Wall -Wno-unused-function -Wextra -march=native -mtune=native
+CXXFLAGS += -mavx -std=c++11 -pedantic -O3 -Wall -Wno-unused-function -Wextra -march=native -mtune=native
 
 union: union.o
 	$(CXX) $(CXXFLAGS) union.o -o union.exe
--- union.cc
+++ union.cc
@@ -7,30 +7,44 @@
 
 using namespace std;
 
-const int MAX_INT = 100000;
-const int MAX_SET_SIZE = 500;
+const int MAX_INT = 500000;
+const int MAX_SET_SIZE = 5000;
 const int NUM_SETS = 100;
 
-const char* ALGORITHMS[] = {"stl"};
+const char* ALGORITHMS[] = {"stl", "multiway"};
 
 typedef chrono::high_resolution_clock Clock;
 typedef chrono::milliseconds ms;
 
+vector<int> stl_set_union(vector<int> sets[]) {
+   vector<int> result(sets[0]);
+   vector<int> tmp;
+
+   for (int i = 1; i < NUM_SETS; ++i) {
+     set_union(result.begin(), result.end(),
+               sets[i].begin(), sets[i].end(),
+               back_inserter(tmp));
+     result = tmp;
+     tmp.clear();
+   }
+   return result;
+}
+
+vector<int> multiway_set_union(vector<int> sets[]) {
+  vector<int>::iterator its[NUM_SETS];
+  for (int i = 0; i < NUM_SETS; ++i) {
+    its[i] = sets[i].begin();
+  }
+
+  vector<int> result;
+  return result;
+}
+
 vector<int> set_union(vector<int> sets[], string algorithm) {
   if (algorithm == "stl") {
-    vector<int> result(sets[0]);
-    vector<int> tmp;
-
-    for (int i = 1; i < NUM_SETS; ++i) {
-      set_union(result.begin(), result.end(),
-                sets[i].begin(), sets[i].end(),
-                back_inserter(tmp));
-      result = tmp;
-      tmp.clear();
-    }
-
-    return result;
-
+    return stl_set_union(sets);
+  } else if (algorithm == "multiway") {
+    return multiway_set_union(sets);
   } else {
     throw runtime_error("unknown algorithm");
   }
@@ -43,9 +57,7 @@ void print_set(vector<int> set) {
   cout << endl;
 }
 
-int main(int argc, char* argv[]) {
-  vector<int> sets[NUM_SETS];
-
+void populate_sets(vector<int> sets[]) {
   random_device rd;
   mt19937 gen(rd());
   uniform_int_distribution<> distribution(1, MAX_INT);
@@ -57,13 +69,34 @@ int main(int argc, char* argv[]) {
     }
     sets[set_i].insert(sets[set_i].end(), tmp.begin(), tmp.end());
   }
+}
 
+int main(int argc, char* argv[]) {
+  vector<int> sets[NUM_SETS];
+  cout << "building sets..." << endl;
+  populate_sets(sets);
+
+  vector<int> reference;
   for (const char* alg : ALGORITHMS) {
-    cout << "using algorithm " << alg << endl;
+    cout << "using algorithm " << alg << "..." << endl;
+
     Clock::time_point start = Clock::now();
     vector<int> output = set_union(sets, string(alg));
     Clock::time_point end = Clock::now();
+
     ms time = chrono::duration_cast<ms>(end - start);
-    cout << "time: " << time.count() << "ms" << endl << endl;
+    cout << "time: " << time.count() << "ms" << endl;
+
+    if (reference.empty()) {
+      reference = output;
+    } else {
+      if (reference == output) {
+        cout << "output is correct" << endl;
+      } else {
+        cout << "OUTPUT DOES NOT MATCH" << endl;
+      }
+    }
+
+    cout << endl;
   }
 }
